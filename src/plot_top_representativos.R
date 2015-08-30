bd1a981b30eadd29740f02a13b71faae35b6c029
fabiomorais <fabio.jorge@gmail.com>
2015-08-30 13:54:12 -0300

    Novo plot

--- src/plot_top_representativos.R
+++ src/plot_top_representativos.R
@@ -17,16 +17,13 @@ desq_freq = unique(df_plot$freq)[1:5]
 df_plot$destaque = df_plot$freq %in% desq_freq
 
 p = ggplot(df_plot, aes(as.numeric(nome_dep), freq))
-p = p + geom_jitter(size = 3, color = "gray")
+p = p + geom_jitter(size = 3, color = "gray70", alpha = 0.5)
 p = p + geom_text(show_guide = TRUE, angle = 35, vjust=-0.25, hjust=0, data = filter(df_plot, destaque == TRUE), aes(x=as.numeric(nome_dep), y=freq, label=nome_dep, colour=toupper(partido_dep)), position = "jitter", size = 6)
-#p = p + guides(colour = guide_legend(override.aes = list(size = 5, shape = c(utf8ToInt("F"))))
-#p = p + scale_colour_discrete("Partido", guide=guide_legend(override.aes=list(size=4)))
-p = p + scale_y_continuous(labels = percent, limit = c(0,0.11))
+p = p + scale_colour_hue("Partido", guide=guide_legend(override.aes=list(size=10, shape = utf8ToInt("A"))))
+p = p + scale_y_continuous(labels = percent, limit = c(0,0.115))
 p = p + xlab(NULL) + ylab("Representatividade")
 p = p + theme_bw()
 p = p + theme(axis.ticks = element_blank(), axis.text.x = element_blank()) + theme(text = element_text(size=20))
-p = p + guides(size = guide_legend(override.aes = list(colour = "black", shape = utf8ToInt("N"))))
-#p = p + scale_size(range = c(2, 10))
 print(p)
 
 library(grid)
